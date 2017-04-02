f3c93c790831cfd19e5caead4ec06b685380d523
Spigell 7th <spigelly@gmail.com>
2017-04-03 01:49:41 +0300

    added files

---
+++ README.md
@@ -0,0 +1,32 @@
+# SYNOPSIS
+
+Add user to Virtualbox's VM according with Vagrant user [guide](https://www.vagrantup.com/docs/boxes/base.html#quot-vagrant-quot-user) and repackage vbox into vagrant box.
+
+# INSTALL
+
+    $ sparrow plg install vagrant-vbox-importer
+
+## Dependencies
+
+### Optional
+
+[sshpass](https://sourceforge.net/projects/sshpass) - for noninteractive password input 
+
+# USAGE
+
+## via yaml
+
+    $ sparrow project add vagrant
+    $ sparrow task add vagrant create vagrant-vbox-importer
+    $ sparrow task ini vagrant/create 
+```
+
+```
+
+# Parameters
+
+
+# Author
+
+[Spigell](mailto:spigell@gmail.com)
+
---
+++ story.bash
@@ -0,0 +1,64 @@
+host_ip=$(config host.ip)
+host_port=$(config host.port)
+host_user=$(config host.user)
+host_password=$(config host.password)
+
+vagrant_user=$(config vagrant.user)
+vagrant_pass=$(config vagrant.password)
+vagrant_key=$(config vagrant.public_key)
+vagrant_key_file=$(config vagrant.public_key_file)
+vagrant_private_key_file=$(config vagrant.private_key_file)
+
+box_name=$(config box.name)
+box_base=$(config box.base)
+
+
+if [[ ! $box_base ]];then
+  echo "no specified box_base"
+  exit 3
+fi
+
+if [[ -z $host_ip ]]; then
+  echo "No ip given"
+  exit 2
+fi
+
+if [[ $vagrant_key_file ]] && [[ $vagrant_key ]]; then
+  echo "allowed only one parameter: vagrant_key_file or vagrant_key"
+  exit 1
+elif [[ ! $vagrant_key_file ]] && [[ ! $vagrant_key ]];then
+  echo "No public key for $vagrant_user specified. You can access to vagrant box via password method only."
+  password_only=yes
+fi
+
+
+if [[ $vagrant_key_file ]]; then 
+  vagrant_key=$(cat $vagrant_key_file)
+fi
+
+if [[ $vagrant_private_key_file ]] && [[ $vagrant_key ]]; then
+  ssh_check_key="-i $vagrant_private_key_file"
+elif [[ ! $vagrant_private_key_file ]]; then
+  echo "private key file not specified. ssh check will be using password method."
+fi  
+
+if [[ $host_password ]];then
+  echo $host_password > /tmp/pass.txt
+  sshpass_opt='sshpass -f /tmp/pass.txt'
+fi
+
+$sshpass_opt ssh -t -o StrictHostKeyChecking=no $host_user@$host_ip -p $host_port 'sudo useradd "'$vagrant_user'" -m; \
+  echo ""'$vagrant_user'":"'$vagrant_pass'"" | sudo chpasswd ; \
+  echo ""'$vagrant_user'" ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers &> /dev/null; \
+  if [[ "'$password_only'" == yes ]]; then exit 0 ; fi; \
+  sudo mkdir -m 700 /home/"'$vagrant_user'"/.ssh 2> /dev/null; \
+  sudo chown "'$vagrant_user'" /home/"'$vagrant_user'"/.ssh ; \
+  echo "'$vagrant_key'" | sudo tee /home/"'$vagrant_user'"/.ssh/authorized_keys &> /dev/null; \
+  sudo chown "'$vagrant_user'" /home/"'$vagrant_user'"/.ssh/authorized_keys ;' &> /dev/null 
+
+rm -rf /tmp/pass.txt
+
+ssh -o StrictHostKeyChecking=no $vagrant_user@$host_ip $ssh_check_key ' exit ' && echo "ok user added"
+
+
+#vagrant package --base $box_base --output $box_name 
